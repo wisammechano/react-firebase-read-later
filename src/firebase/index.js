85b8bfbd218360977c8a76510634fb06e4cc4bd1
Wisam Naji <guitar90rocker@gmail.com>
2022-06-14 15:18:15 +0300

    added fb config

---
+++ src/firebase/index.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const fbConfig = {
+  // @TODO: Insert your firebase config
+};
+
+// Initialize Firebase
+const app = initializeApp(fbConfig);
+
+export default app;
